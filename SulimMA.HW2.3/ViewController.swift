7ccc91d3317ba54156fb91745ad01d25ca0fe55c
Mikhail <s_m86@mail.ru>
2021-03-30 14:59:06 +0300

    Initial Commit

---
+++ SulimMA.HW2.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SulimMA.HW2.3
+//
+//  Created by михаил сулим on 30.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
