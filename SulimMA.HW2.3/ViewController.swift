f9078ef3fc36aac7bb4cb8aea3ddd21aa3b3a5f6
Mikhail <s_m86@mail.ru>
2021-03-30 20:02:22 +0300

    HW2.3.1

--- SulimMA.HW2.3/ViewController.swift
+++ SulimMA.HW2.3/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var userNameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        userNameText.delegate = self
+        userNameText.delegate = self
+        
     }
-
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        passwordText.becomeFirstResponder()
+    }
+    
+    @IBAction func forgotUaerNameAlert() {
+        showAlertForgotUserData(with: "Oops!", and: "Your User Name is User")
+        return
+    }
+    @IBAction func forgotPasswordAlert() {
+        showAlertForgotUserData(with: "Oops!", and: "Your Password is Password")
+        return
+    }
+    
 
 }
-
+extension ViewController {
+    private func showAlertForgotUserData(with title: String, and messege: String) {
+        let alert = UIAlertController(title: title, message: messege, preferredStyle: .alert)
+        present(alert, animated: true)
+        let defaultAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(defaultAction)
+    }
+}
---
+++ SulimMA.HW2.3/WelcomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeViewController.swift
+//  SulimMA.HW2.3
+//
+//  Created by михаил сулим on 30.03.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
